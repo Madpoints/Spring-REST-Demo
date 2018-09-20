b644e85be4fe0f977b74269dfa1e12d6bb507faa
John Furtado <jf8846@gmail.com>
2018-09-20 18:32:56 -0400

    config class

---
+++ src/main/java/com/madpoints/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package com.madpoints.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.madpoints.springdemo")
+public class DemoAppConfig {
+
+}
