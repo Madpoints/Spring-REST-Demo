5b55b92e3692ab133eef970db45fa0e8231d75b6
John Furtado <jf8846@gmail.com>
2018-09-22 19:46:59 -0400

    catch all exception handler

--- src/main/java/com/madpoints/springdemo/rest/StudentRestController.java
+++ src/main/java/com/madpoints/springdemo/rest/StudentRestController.java
@@ -64,4 +64,17 @@ public class StudentRestController {
 		
 		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
 	}
+	
+	// catchall exception handler
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(Exception exception) {
+		
+		StudentErrorResponse error = new StudentErrorResponse();
+		
+		error.setStatus((HttpStatus.BAD_REQUEST.value()));
+		error.setMessage(exception.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
 }
